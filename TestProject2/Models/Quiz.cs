e65e6212bba2cdf7e9c8249e84ba201b13ce0f8a
Hambodini <trimeyers1@gmail.com>
2022-09-24 21:47:44 -0500

    added classes for tests quizes and questions. changed index page added resticted teacher only view

--- TestProject2/Models/Quiz.cs
+++ TestProject2/Models/Quiz.cs
@@ -1,11 +1,11 @@
-﻿sing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TestProject2.Models
+namespace TestProject.Models
 {
-    public class Student
+    public class Quiz:Test
     {
     }
 }
