3722650e67b21fa88d98ab699f41525d219def3e
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-09-26 01:03:44 -0500

    StudentPortal now displays current logged on users id and list of associated student ids if it is a guardian

--- TestProject2/Controllers/HomeController.cs
+++ TestProject2/Controllers/HomeController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestProject2.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 
 namespace TestProject2.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +34,24 @@ namespace TestProject2.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult StudentPortal()
+        {
+            var students = new List<Guardian>();
+            var id = User.Identity.GetUserId();
+            var user = _context.Users.Find(id);
+
+            if (user.Role == "Guardian")
+            {
+                students = _context.Guardians.Where(u => u.UserId == id).ToList();
+            }
+
+            var spvm = new StudentPortalViewModel();
+            spvm.User = user;
+            spvm.StudentIds = students;
+
+            return View(spvm);
         }
     }
 }
---
+++ TestProject2/Models/StudentPortalViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject2.Models
+{
+    public class StudentPortalViewModel
+    {
+        public ApplicationUser User { get; set; }
+        public List<Guardian> StudentIds { get; set; }
+    }
+}
