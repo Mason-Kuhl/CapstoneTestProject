c2e16a0e71bbb5a3b1b75fc95d5918991172c55d
Mason-Kuhl <Mason.Kuhl@my.southeast.edu>
2022-09-27 16:08:35 -0500

    Made DrillViewModel Class

---
+++ TestProject2/Models/DrillViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestProject2.Models
+{
+    public class DrillViewModel
+    {
+        public int StudentId { get; set; }
+        public int CurrentTier { get; set; }
+        public List<Question> DrillQuestions { get; set; }
+    }
+}
